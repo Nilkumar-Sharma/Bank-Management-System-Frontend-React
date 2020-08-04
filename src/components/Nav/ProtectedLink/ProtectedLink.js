43428f32c3c401b044bfab1a944245dcdc360ca5
Nilkumar Sharma <nilkumarsharma2016@gmail.com>
2020-08-04 13:58:40 +0530

    added unit test cases for ProtectedLink Component

--- src/components/Nav/ProtectedLink/ProtectedLink.js
+++ src/components/Nav/ProtectedLink/ProtectedLink.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { useSelector } from 'react-redux';
-const ProtectedLink=React.memo(props=>{
+const ProtectedLink=(props=>{
 
-    const isLoggedIn = useSelector(state => state.loggedIn)
-    
+    let { loggedIn } = useSelector(state => state)
+    // console.log(loggedIn)
+    // if (loggedIn.loggedIn) loggedIn = loggedIn.loggedIn;
+    // console.log(props.protect === 'afterLogIn' && loggedIn === true)
+    // console.log(((((props.protect === 'afterLogIn') && loggedIn === true) ) )?"SHow":"Hide")
     try {
         return (
-            ((props.protect === 'afterLogIn' && isLoggedIn) || (props.protect === 'beforeLogIn' && !isLoggedIn) )  &&
+            (props.protect === 'both' || ((props.protect === 'afterLogIn') && loggedIn) || (props.protect === 'beforeLogIn' && !loggedIn)) &&
             <li className="nav-item active">
                 <Link className="nav-link" to={props.to}>{props.name}</Link>
             </li>
+        
         )
-}catch(error) {
-    console.log(Error)
+    } catch (error) {
+        console.log(Error)
+        return ("ollla")
+
+        
 }
 
 });
--- src/components/Nav/ProtectedLink/ProtectedLink.test.js
+++ src/components/Nav/ProtectedLink/ProtectedLink.test.js
@@ -1,20 +1,73 @@
-// import {configure,shallow} from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16'
-// import ProtectedLink from './ProtectedLink'
-// import React from 'react';
-// import { Provider } from 'react-redux';
-// import { store } from '../../../index';
+import {configure,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import ProtectedLink from './ProtectedLink'
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store' //ES6 modules
+import thunk from 'redux-thunk';
+import { Link } from 'react-router-dom'
+import  rootReducer  from '../../../store/reducers/reducer'
+import { createStore, applyMiddleware } from 'redux'
+// import { createShallow } from '@material-ui/core/test-utils';
+import * as Redux from 'react-redux'
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
-// describe('<ProtectedLink/>', () => {
-//     it('it should have  Protected Link when logged In', () => {
-//         const wrapper = shallow(<Provider store={store}><ProtectedLink /></Provider> );
-//         expect(wrapper.find(ProtectedLink)).toHaveLength(2);
-        
-//     });
-//     it('sums numbers', () => {
-//         expect(1 + 2).toEqual(3);
-//         expect(2 + 2).toEqual(4);
-//     });
-// })
+const middlewares = [thunk]
+
+describe('<ProtectedLink/>', () => {
+    let useSelectorSpy: jest.SpyInstance;
+    let useDispatchSpy: jest.SpyInstance;
+    beforeEach(() => {
+        useSelectorSpy = jest.spyOn(Redux, 'useSelector');
+        // shallow = createShallow();
+        // useSelectorSpy = jest.spyOn(Redux, 'useSelector');
+        useDispatchSpy = jest.spyOn(Redux, 'useDispatch');
+    })
+    
+
+    it('Links with protect="afterLogIn" should not  be shown when loggedIn = false ', () => {
+        useSelectorSpy.mockReturnValue({
+            loggedIn: false
+        })
+        const wrapper = shallow(<ProtectedLink protect='afterLogIn' to='/' name='dummyName' ></ProtectedLink>);
+        console.log(wrapper.debug())
+        expect(wrapper.find(Link).length).toEqual(0);
+    });
+
+    it('Links with protect="afterLogIn" should  be shown when loggedIn = true ', () => {
+            useSelectorSpy.mockReturnValue({
+                loggedIn: true
+            })
+        const wrapper = shallow(<ProtectedLink protect='afterLogIn' to='/' name='dummyName' ></ProtectedLink>);
+        expect(wrapper.find(Link).length).toEqual(1);
+    });
+ 
+
+    it('Links with protect="beforeLogIn" should  be not shown when loggedIn =true', () => {
+        useSelectorSpy.mockReturnValue({
+            loggedIn: true
+        })
+        const wrapper = shallow(<ProtectedLink protect='beforeLogIn' to='/' name='dummyName' ></ProtectedLink>);
+        expect(wrapper.find(Link).length).toEqual(0);
+
+    });
+    it('Links with protect="beforeLogIn" should  be  shown when loggedIn = false', () => {
+        useSelectorSpy.mockReturnValue({
+            loggedIn: false
+        })
+        const wrapper = shallow(<ProtectedLink protect='beforeLogIn' to='/' name='dummyName' ></ProtectedLink>);
+        expect(wrapper.find(Link).length).toEqual(1);
+    });
+    it('Links with protect="both" should  be shown irrespective of loggedIn state', () => {
+        useSelectorSpy.mockReturnValue({
+            loggedIn: false
+        })
+        const wrapper = shallow(<ProtectedLink protect='both' to='/' name='dummyName' ></ProtectedLink>);
+        expect(wrapper.find(Link).length).toEqual(1);
+    });
+
+
+
+});
+  
